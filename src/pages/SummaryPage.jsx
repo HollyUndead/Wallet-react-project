2d6c26653c7979ee040b934b6c5e98398ebc52ca
VickyBondarenko <quikyv@gmail.com>
2023-04-01 21:39:45 +0200

    Add summary base style

--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -1,3 +1,38 @@
+import styled from 'styled-components';
+
 export const SummaryPage = () => {
-  return <h1>Summary</h1>;
+  return (
+    <SummaryWraper>
+      <SummaryTitle>Statistics</SummaryTitle>
+    </SummaryWraper>
+  );
 };
+
+const SummaryWraper = styled.div`
+  width: '100%';
+  padding-top: 43px;
+  @media screen and (min-width: 768px) {
+    padding-top: 20px;
+  }
+  @media screen and (min-width: 1200px) {
+    padding-top: 32px;
+  }
+`;
+
+const SummaryTitle = styled.h1`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 1.5;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #000000;
+  padding-bottom: 8px;
+  @media screen and (min-width: 768px) {
+    padding-bottom: 20px;
+  }
+`;
