cadaf50e2b9ec44025293408a3ec5c065ba3335d
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-04-01 12:13:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} loading={null}>
-      <BrowserRouter basename="/Wallet-react-project">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
