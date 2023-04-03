8c83aa22da7acb25723a2302a4b2418ed7a84757
VickyBondarenko <quikyv@gmail.com>
2023-04-03 18:49:47 +0200

    Add navigate

--- src/pages/CurrencyPage.jsx
+++ src/pages/CurrencyPage.jsx
@@ -1,11 +1,16 @@
 import { Currency } from '../components/Currency/Currency';
 import { useMediaQuery } from 'react-responsive';
+import { Navigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CurrencyPage = () => {
   const isMobile = useMediaQuery({
     query: '(max-width: 767px)',
   });
+  if (!isMobile) {
+    return <Navigate to="/" />;
+  }
+
   return <CurrencyWrapper>{isMobile && <Currency />}</CurrencyWrapper>;
 };
 
