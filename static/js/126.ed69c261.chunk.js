fc88751863251c96a6b401384c9b7e588c71f9b2
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-04-03 21:29:48 +0000

    Deploying to gh-pages from @ HollyUndead/Wallet-react-project@049dcbd0c379c896f3eb0bac6f665b306c89cc00 🚀

---
+++ static/js/126.ed69c261.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwallet_react_project=self.webpackChunkwallet_react_project||[]).push([[126],{6126:function(e,t,n){n.r(t);var r,a=n(168),c=n(8473),u=n(4805),s=n(7689),i=n(4313),l=n(184),p=i.ZP.div(r||(r=(0,a.Z)(["\n  padding-top: 32px;\n  display: flex;\n  justify-content: center;\n"])));t.default=function(){var e=(0,u.useMediaQuery)({query:"(max-width: 767px)"});return e?(0,l.jsx)(p,{children:e&&(0,l.jsx)(c.F,{})}):(0,l.jsx)(s.Fg,{to:"/"})}}}]);
+//# sourceMappingURL=126.ed69c261.chunk.js.map
